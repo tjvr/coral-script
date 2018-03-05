e946684db45a7e0e58401556093df8ad8d6e3d03
tjvr <tim@tjvr.org>
2018-03-05 22:30:35 +0000

    add account_type filter

--- go-monzo/accounts.go
+++ go-monzo/accounts.go
@@ -10,11 +10,15 @@ type Account struct {
 	AccountNumber string `json:"account_number"`
 }
 
-func (cl *Client) Accounts() ([]*Account, error) {
+func (cl *Client) Accounts(accountType string) ([]*Account, error) {
+	args := map[string]string{}
+	if accountType != "" {
+		args["account_type"] = accountType
+	}
 	rsp := &struct {
 		Accounts []*Account `json:"accounts"`
 	}{}
-	if err := cl.request("GET", "/accounts", nil, rsp); err != nil {
+	if err := cl.request("GET", "/accounts", args, rsp); err != nil {
 		return nil, err
 	}
 	return rsp.Accounts, nil
